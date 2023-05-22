d1bede72ffcedcc4e34c469a22badfcc4254946e
sergionahas <nahassergio14@gmail.com>
2023-05-22 18:23:29 -0400

    iteration API endpoint

--- backend.py
+++ backend.py
@@ -17,7 +17,7 @@ class EmailRequest(BaseModel):
     prompt: str
 
 class SalesData(BaseModel):
-    email: str
+    email_address: str
     context: str
     sell_product: str
     example_emails: str
@@ -25,6 +25,11 @@ class SalesData(BaseModel):
 class ReplyData(BaseModel):
     email: str
     email_address: str
+
+class FeedbackData(BaseModel):
+    email_address: str
+    original_response: str
+    feedback: str
     
 def generate_response(system_prompt, user_prompt, *args):
     import openai
@@ -110,10 +115,29 @@ def generate_sales_prompt(data):
     sell_product = data.sell_product
     example_emails = data.example_emails
     
-    prompt = f"The email address is {email_address}. \
+    prompt = f"The email address is {email}. \
                Here is the context: {context}. \
                This is what I am trying to sell: {sell_product} \
                Here are examples of emails that have been successful at getting replies: {example_emails}. \
+               Always specify the Email subject line in the first line of your response with the following format: Subject: <INSERT_SUBJECT_HERE>. \
+               an example of spacing would be this: \
+               Dear Bob, <br><br> \
+               I hope this email finds you well. \
+               I came across your profile and noticed your passion for playing the piano and finding a fun job. As an AI Sales Development Representative, I have the perfect solution for you. <br><br> \
+               I believe our AI tool can help you find your dream job and make your job search process more efficient. If you're interested, I would love to schedule a demo and show you how our tool works. <br><br> \
+               Best regards, <br><br> \
+               Steve Joseph <br><br>"
+    
+    return prompt
+
+def generate_feedback_prompt(data):
+    email = data.email_address
+    feedback = data.feedback
+    response = data.original_response
+    
+    prompt = f"The email address is {email}. \
+               Here is the feedback: {feedback}. \
+               This was the original response that needs improvement based on the feedback: {response} \
                Always specify the Email subject line in the first line of your response with the following format: Subject: <INSERT_SUBJECT_HERE>. \
                an example of spacing would be this: \
                Dear Bob, <br><br> \
@@ -133,7 +157,16 @@ def generate_sales_response(sales_data: SalesData):
         prompt,
     )
     return {"email": response, "email_address": sales_data.email_address}
-       
+
+@app.post("/sales/iterate")
+def generate_feedback_response(feedback_data: FeedbackData):
+    prompt = generate_feedback_prompt(feedback_data)
+    response = generate_response(
+        """You are an AI Sales Development Representative who needs to improve a personalized sales email for the user based on their feedback.""",
+        prompt,
+    )
+    return {"email": response, "email_address": feedback_data.email_address}
+
 @app.post("/sales/send")
 def send_email(reply_dict: ReplyData):
     try:
